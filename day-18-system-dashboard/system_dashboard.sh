4f51d915c425f17941311401d41d8c8af1ab60b1
Isrcode1 <folarinisrael981@gmail.com>
2026-03-17 17:17:06 +0100

    feat: add disk usage alert to dashboard

--- day-18-system-dashboard/system_dashboard.sh
+++ day-18-system-dashboard/system_dashboard.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
 echo ""
-echo "------ SYSTEM LOAD ------"
-uptime
+echo "------ DISK ALERT CHECK ------"
+DISK_USAGE=$(df / | awk 'END{print $5}' | sed 's/%//')
+
+echo "Disk Usage: $DISK_USAGE%"
+
+if [ "$DISK_USAGE" -gt 50 ]; then
+    echo "WARNING: Disk usage is high!"
+else
+    echo "Disk usage is normal."
+fi
