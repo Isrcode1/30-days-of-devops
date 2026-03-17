e46d693e55e90abc4f704c31d36ff86d00ea0bcd
Isrcode1 <folarinisrael981@gmail.com>
2026-03-17 16:49:28 +0100

    feat: add system load section to dashboard

--- day-18-system-dashboard/system_dashboard.sh
+++ day-18-system-dashboard/system_dashboard.sh
@@ -1,38 +1,3 @@
-#!/bin/bash
-
-clear
-
-echo "======================================="
-echo "        DEVOPS SYSTEM DASHBOARD"
-echo "======================================="
-echo "Hostname: $(hostname)"
-echo "Date: $(date)"
 echo ""
-
-echo "------ SYSTEM UPTIME ------"
+echo "------ SYSTEM LOAD ------"
 uptime
-echo ""
-
-echo "------ CPU USAGE ------"
-top -bn1 | grep "Cpu(s)"
-echo ""
-
-echo "------ MEMORY USAGE ------"
-free -h
-echo ""
-
-echo "------ DISK USAGE ------"
-df -h | grep '^/dev'
-echo ""
-
-echo "------ TOP 5 CPU PROCESSES ------"
-ps -eo pid,cmd,%mem,%cpu --sort=-%cpu | head -n 6
-echo ""
-
-echo "------ RECENT ALERTS ------"
-tail -n 5 /mnt/c/Users/folarin/30-days-of-devops/day-16-log-analysis/analysis_report.log 2>/dev/null
-
-echo ""
-echo "======================================="
-echo "        END OF DASHBOARD"
-echo "======================================="
